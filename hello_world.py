08f00d457768a6636030d8a27b37e4832f4586bc
s11607 <s11607@gmail.com>
2025-03-19 17:46:48 +0800

    添加hello_world.py文件

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def hello():
+    """打印欢迎消息"""
+    print("Hello, World!")
+    print("你好，世界！")
+    print("こんにちは世界！")
+
+if __name__ == "__main__":
+    hello()
